521a4500f0b9eff6b9b2d1a1f37cfa48dd671360
Alyson Paige <alysonstiffel@gmail.com>
2018-06-11 12:39:11 -0400

    shuffle cards

--- js/app2.js
+++ js/app2.js
@@ -15,7 +15,7 @@ function generateCard(card) {
 
 function initGame() {
 	var deckOfCards = document.querySelector('.deck');
-	var cardHTML = cardArr.map(function(card) {
+	var cardHTML = shuffle(cardArr).map(function(card) {
 		return generateCard(card);
 	});
 	deckOfCards.innerHTML = cardHTML.join('');
@@ -51,3 +51,18 @@ allCards.forEach(function(card) {
 		}
 	});
 });
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
